65adbb4306c9b4355f140b65e88559c7bde95ec7
Mumin Ali <muminali13@hotmail.co.uk>
2025-05-13 18:31:51 +0100

    addCustomerAddress checks required fields, returns new address if successfully added, success boolean is always returned

--- controllers/addresses.js
+++ controllers/addresses.js
@@ -4,7 +4,7 @@ async function getCustomerAddress(req, res) {
     const { customer_id } = req.params;
 
     if (!customer_id) {
-        res.status(400).send({error: 'No customer id provided'});
+        res.status(400).send({success: false, error: 'No customer id provided'});
     }
 
     const { data, error } = await supabase
@@ -14,15 +14,24 @@ async function getCustomerAddress(req, res) {
 
     if (error) {
         console.error(error);
-        return res.status(400).json({error: "Failed to get customer address", fullError: error});
+        return res.status(400).json({success: false, error: "Failed to get customer address", fullError: error});
     }
 
-    return res.status(200).json(data);
+    return res.status(200).json({success: true, data: data});
 }
 
 async function addCustomerAddress(req, res) {
 
-    const { customer_id, line1, line2, town, postcode } = req.body;
+    const { customer_id, line1, line2, town, postcode, delivery_notes, is_default } = req.body;
+
+    if (!customer_id)
+        return res.status(400).json({ success: false, error: 'Missing required fields: customer_id' });
+    if (!line1)
+        return res.status(400).json({ success: false, error: 'Missing required fields: line1' });
+    if (!town)
+        return res.status(400).json({ success: false, error: 'Missing required fields: town' });
+    if (!postcode)
+        return res.status(400).json({ success: false, error: 'Missing required fields: postcode' });
 
     const { data, error } = await supabase
         .from('addresses')
@@ -31,15 +40,17 @@ async function addCustomerAddress(req, res) {
             line1,
             line2,
             town,
-            postcode
-    })
+            postcode,
+            delivery_notes
+        })
+        .select()
 
     if (error) {
         console.error(error);
-        return res.status(400).json({error: "Failed to add customer address", fullError: error});
+        return res.status(400).json({ success:false, error: "Failed to add customer address", fullError: error});
     }
 
-    return res.status(200).json(data);
+    return res.status(200).json({ success: true, data:data});
 }
 
 module.exports = {
